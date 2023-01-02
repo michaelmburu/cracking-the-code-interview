88597d87b08e1ef25e36402ec3121fd6e5b9042a
Michael Mburu <ichangaimichael@yahoo.com>
2023-01-02 11:00:03 +0300

    - Linked List

---
+++ LinkedList.cs
@@ -0,0 +1,39 @@
+﻿namespace CrackingTheCodeInterview;
+public class Node
+{
+    Node? next = null;
+    int data;
+
+    public Node(int d)
+    {
+        data = d;
+    }
+
+    void AppendToTail(int d)
+    {
+        Node end = new Node(d);
+        Node n = this;
+        while(n.next != null)
+        {
+            n = n.next;
+        }
+
+        n.next = end;
+    }
+
+    Node deleteNode(Node head, int d)
+    {
+        Node n = head;
+
+        while(n.next != null)
+        {
+            if (n.data == d)
+            {
+                n.next = n.next.next;
+                return head;
+            }
+        }
+        return head;
+    }
+}
+
