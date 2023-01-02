64e27ba5609d3dbd20a68ee56e46e501f9430811
Michael Mburu <ichangaimichael@yahoo.com>
2023-01-02 11:43:30 +0300

    - Queue, Stack

---
+++ Queue.cs
@@ -0,0 +1,65 @@
+﻿using System;
+namespace CrackingTheCodeInterview
+{
+    public class Queue<T>
+    {
+        private class QueueNode<T>
+        {
+            T data;
+            QueueNode<T> next;
+
+            public QueueNode(T data)
+            {
+                this.data = data;
+            }
+
+
+             QueueNode<T> first;
+             QueueNode<T> last;
+
+            public void add(T item)
+            {
+                QueueNode<T> t = new QueueNode<T>(item);
+                if (last != null)
+                {
+                    last.next = t;
+                }
+            }
+
+            public T remove()
+            {
+                if(first == null)
+                {
+                    throw new Exception();
+                }
+                T data = first.data;
+                first = first.next;
+                if(first == null)
+                {
+                    last = null;
+                }
+
+
+                return data;
+            }
+
+            public T peek()
+            {
+                if(first == null)
+                {
+                    throw new Exception();
+                }
+
+                return first.data;
+            }
+
+            public bool isEmpty()
+            {
+                return first == null;
+            }
+        }
+
+    
+    }
+}
+
---
+++ Stack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace CrackingTheCodeInterview
+{
+    public class Stack<T>
+    {
+        private class StackNode<T>
+        {
+            T data;
+            StackNode<T> next;
+
+            public StackNode(T data)
+            {
+                this.data = data;
+            }
+
+
+            StackNode<T> top;
+
+            public T pop()
+            {
+                if (top == null) throw new Exception();
+                T item = top.data;
+
+                top = top.next;
+                return item;
+            }
+
+            public void push(T item)
+            {
+                StackNode<T> t = new StackNode<T>(item);
+                t.next = top;
+                top = t;
+            }
+
+            public T peek()
+            {
+                if(top == null)
+                {
+                    throw new Exception();
+                }
+
+                return top.data;
+            }
+
+            public bool isEmpty()
+            {
+                return top == null;
+            }
+        }       
+    }
+}
+
